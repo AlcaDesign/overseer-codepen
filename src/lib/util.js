3116062d9a5109a226dcdde9ef0200ec11597b64
Jacob Foster <jfost3@gmail.com>
2025-10-14 14:23:09 -0500

    Update util.js

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,7 +3,7 @@ export function formatUsername(user) {
 	if(user.login && display.toLowerCase().replaceAll(/\s/g, '') !== user.login) {
 		return `${display} (${user.login})`;
 	}
-	return display || '(unknown)';
+	return user.display || user.login || '(unknown)';
 }
 export function getBadgeSubTier(data) {
 	return data.isPrime ? 'P' : `${data.tier}`;
@@ -19,4 +19,28 @@ export function n(value) {
 }
 export function fmt(value, word = '', pluralForm = '') {
 	return `${value.toLocaleString()} ${s(value, word, pluralForm)}`;
+}
+export function getBadgeSubIsHighlighted(data) {
+	const value = typeof data === 'number' ? data : data.cumulativeMonths;
+	return value % 12 === 0 || value === 1;
+}
+export function getBadgeSubMonths(data) {
+	return `${n(data.cumulativeMonths)}m`;
+}
+export function getBadgeSubMonthsTitle(data) {
+	const months = typeof data === 'number' ? data : data.cumulativeMonths;
+	const years = Math.floor(months / 12);
+	const monthsRemainder = months % 12;
+
+	const monthsFormatted = fmt(months, 'month');
+	const yearsFormatted = fmt(years, 'year');
+	const monthsRemainderFormatted = fmt(monthsRemainder, 'month');
+
+	if(monthsRemainder === 0) {
+		return `${monthsFormatted} (${yearsFormatted})`;
+	}
+	else if(years === 0) {
+		return `${monthsFormatted}`;
+	}
+	return `${monthsFormatted} (${yearsFormatted} ${monthsRemainderFormatted})`;
 }
